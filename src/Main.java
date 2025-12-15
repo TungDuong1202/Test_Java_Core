f72c098a5aefa37ec12389c62bdaf32c1d4cc169
TungDuong1202 <phanduong122004@gmail.com>
2025-12-15 15:02:58 +0700

    submit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class Main {
+
+    public static List<String> processStrings(List<String> list){
+        List<String> resultList = new ArrayList<>();
+        for (String element : list){
+            if (element.contains("@")){
+                int lastAtIndex = element.lastIndexOf("@");
+                //Lấy chuỗi trước kí tự @ cuối cùng
+                String beforeAt = element.substring(0, lastAtIndex);
+                //Xóa các kí tự @
+                beforeAt = beforeAt.replace("@","");
+                String processed = beforeAt + "@...";
+                resultList.add(processed);
+            }
+        }
+        return resultList;
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("JavaCore");
+        list.add("List");
+        list.add("ArrayList");
+        list.add("tran.the.dung@vsi-international.com");
+        list.add("dungtt@gmail.com");
+        list.add("d@ung@vsi-international.com");
+        list.add("VietSoftw@re@vsi-international.com");
+        list.add("@@@gmail.com");
+        List<String> result = processStrings(list);
+        for (String element : result){
+            System.out.println(element);
         }
     }
 }
